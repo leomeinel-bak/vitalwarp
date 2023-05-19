53f0b11eebbcee623e65591844c866ca2772afd0
Leopold Johannes Meinel <leo@meinel.dev>
2023-05-19 22:37:59 +0200

    Use non-deprecated getPluginMeta()

--- src/main/java/dev/meinel/leo/vitalwarp/VitalWarp.java
+++ src/main/java/dev/meinel/leo/vitalwarp/VitalWarp.java
@@ -2,7 +2,7 @@
  * File: VitalWarp.java
  * Author: Leopold Meinel (leo@meinel.dev)
  * -----
- * Copyright (c) 2022 Leopold Meinel & contributors
+ * Copyright (c) 2023 Leopold Meinel & contributors
  * SPDX ID: GPL-3.0-or-later
  * URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
  * -----
@@ -35,7 +35,7 @@ public final class VitalWarp extends JavaPlugin {
         messages = new Messages();
         Bukkit
                 .getLogger()
-                .info("VitalWarp v" + this.getDescription().getVersion() + " enabled");
+                .info("VitalWarp v" + this.getPluginMeta().getVersion() + " enabled");
         Bukkit.getLogger().info("Copyright (C) 2022 Leopold Meinel");
         Bukkit.getLogger().info("This program comes with ABSOLUTELY NO WARRANTY!");
         Bukkit
@@ -52,7 +52,7 @@ public final class VitalWarp extends JavaPlugin {
     public void onDisable() {
         Bukkit
                 .getLogger()
-                .info("VitalWarp v" + this.getDescription().getVersion() + " disabled");
+                .info("VitalWarp v" + this.getPluginMeta().getVersion() + " disabled");
     }
 
     private void setupStorage() {
